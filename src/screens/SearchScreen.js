7cc22078635b5df9be2cfb274a67054bc87b7ab9
kanjamad bosel <kanjamadpor@gmail.com>
2020-02-01 22:23:02 -0800

    Starting the SeachBar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
     return (
-        <View>
+        <View style={styles.view}>
+            <SearchBar />
             <Text> Search Screen </Text>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    view:{
+        backgroundColor:'white',
+        height:1000
+    }
+});
 
 export default SearchScreen;
