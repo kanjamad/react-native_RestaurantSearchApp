b1ca24843b298ae316a0517f457b35da070ef513
kanjamad bosel <kanjamadpor@gmail.com>
2020-02-05 12:03:21 -0800

    Making a request

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,16 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+
+    const searchApi = () => {
+        yelp.get('/search')
+    };
+
     return (
         <View style={styles.view}>
             <SearchBar 
@@ -12,7 +19,7 @@ const SearchScreen = () => {
             onTermSubmit={()=> console.log('term was submitted')}
             />
             <Text> Search Screen </Text>
-            <Text>{term}</Text>
+            <Text>We have found {results.length} results </Text>
         </View>
     );
 };
