8a959e517a05e24b33c475642b08bd0b7815db1f
kanjamad bosel <kanjamadpor@gmail.com>
2020-02-06 22:21:03 -0800

    error Handing

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,8 +6,10 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const searchApi = async () => {
+        try {
         const response = await yelp.get('/search', {
             params:{
                 limit:50,
@@ -16,6 +18,9 @@ const SearchScreen = () => {
             }
         });
         setResults(response.data.businesses);
+        } catch (err) {
+            setErrorMessage('Something went wrong');
+        }
     };
 
     return (
@@ -25,7 +30,7 @@ const SearchScreen = () => {
             onTermChange={setTerm}
             onTermSubmit={searchApi}
             />
-            <Text> Search Screen </Text>
+            {errorMessage ? <Text> {errorMessage} </Text> : null}
             <Text>We have found {results.length} results </Text>
         </View>
     );
