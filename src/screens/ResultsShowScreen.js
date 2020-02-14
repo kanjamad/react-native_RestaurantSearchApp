468772ea86ff69024100f73a3c30f18a2c58c2f7
kanjamad bosel <kanjamadpor@gmail.com>
2020-02-14 12:02:00 -0800

    Add Navigation

---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Image, Text, StyleSheet} from 'react-native';
+
+
+const ResultsShowScreen = () => {
+    return (
+        <View>
+        <Text> ResultsShowScreen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default ResultsShowScreen;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,7 +4,8 @@ import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
 
-const SearchScreen = () => {
+const SearchScreen = ({navigation}) => {
+    console.log(props);
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
     // console.log(results);
@@ -23,11 +24,16 @@ const SearchScreen = () => {
             onTermSubmit={()=> searchApi(term)}
             />
             {errorMessage ? <Text> {errorMessage} </Text> : null}
-            <Text>We have found {results.length} results </Text>
             <ScrollView>
-            <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
-            <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
-            <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
+            <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" 
+            navigation={navigation}
+            />
+            <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" 
+            navigation={navigation}
+            />
+            <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" 
+            navigation={navigation}
+            />
             </ScrollView>
         </View>
     );
