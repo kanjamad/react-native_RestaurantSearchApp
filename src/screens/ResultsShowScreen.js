7a7b51cabbf62a87c766274ef006b6b992b877f3
kanjamad bosel <kanjamadpor@gmail.com>
2020-02-14 22:48:09 -0800

    Communicating Between Screens

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -2,7 +2,10 @@ import React from 'react';
 import {View, Image, Text, StyleSheet} from 'react-native';
 
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({navigation}) => {
+    const id = navigation.getParam('id');
+    console.log(id);
+
     return (
         <View>
         <Text> ResultsShowScreen</Text>
